c51b0ae6f9bf77ef1098f0142c432f8e31e2c279
Alejandro Alfonso Niño <alejo1985ing@gmail.com>
2019-03-05 08:54:32 -0500

    Archivos

---
+++ js/main.js
@@ -0,0 +1,129 @@
+$(document).ready(function(){
+    $("#sub_menu1").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en menu");
+        $("#imageId").attr('src', 'img/google_io.png');
+        return false;
+    });
+
+    $("#sub_menu1").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#titulo").html('Google I/O Sessions');
+        return false;
+    });
+
+    $("#sub_menu1").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#texto").html('If you have a chance to attend all of your favorite talks, or you want to refresh what you learned at the conference, you can (re)watch recordings of the Keynotes and Sessions on the I/O website at your convenience.');
+        return false;
+    });
+
+    $("#sub_menu2").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en menu");
+        $("#imageId").attr('src', 'img/gdg.png');
+        return false;
+    });
+
+    $("#sub_menu2").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#titulo").html('Google Developer Groups');
+        return false;
+    });
+
+    $("#sub_menu2").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#texto").html('Global community interested in learning, using and promoting the multiple application technologies offered by Google. It is focused on developers and technical-educational content.');
+        return false;
+    });
+
+    $("#sub_menu3").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en menu");
+        $("#imageId").attr('src', 'img/google_experts.png');
+        return false;
+    });
+
+    $("#sub_menu3").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#titulo").html('Google Developer Expert (GDEs)');
+        return false;
+    });
+
+    $("#sub_menu3").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#texto").html('Are a global network of experienced product strategists, designers, developers and marketing professionals actively supporting developers, startups and companies changing the world through web and mobile applications.');
+        return false;
+    });
+
+    $("#menu4").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en menu");
+        $("#imageId").attr('src', 'img/google_experts.png');
+        return false;
+    });
+
+    $("#menu4").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#titulo").html('Google Developer Expert (GDEs)');
+        return false;
+    });
+
+    $("#menu4").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#texto").html('Are a global network of experienced product strategists, designers, developers and marketing professionals actively supporting developers, startups and companies changing the world through web and mobile applications.');
+        return false;
+    });
+
+    $("#menu2").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en menu");
+        $("#imageId").attr('src', 'img/google_io.png');
+        return false;
+    });
+
+    $("#menu2").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#titulo").html('Google I/O Sessions');
+        return false;
+    });
+
+    $("#menu2").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#texto").html('If you have a chance to attend all of your favorite talks, or you want to refresh what you learned at the conference, you can (re)watch recordings of the Keynotes and Sessions on the I/O website at your convenience.');
+        return false;
+    });
+
+    $("#menu3").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en menu");
+        $("#imageId").attr('src', 'img/gdg.png');
+        return false;
+    });
+
+    $("#menu3").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#titulo").html('Google Developer Groups');
+        return false;
+    });
+
+    $("#menu3").on("click", function(event) {
+        event.preventDefault();
+        console.log("click en de titulo");
+        $("#texto").html('Global community interested in learning, using and promoting the multiple application technologies offered by Google. It is focused on developers and technical-educational content.');
+        return false;
+    });
+   
+
+});
